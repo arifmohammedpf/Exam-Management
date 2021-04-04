9fbab205b2e579c38fbf2049d6ed0fc9d702240d
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2021-04-04 09:13:29 +0530

    Code Refactor : - ExcelStudent.cs

--- Exam Cell/Forms/ExcelStudents.cs
+++ Exam Cell/Forms/ExcelStudents.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Exam_Cell
+﻿namespace Exam_Cell
 {
     public class ExcelStudents
     {   //give column name from sql table
@@ -13,6 +7,5 @@ namespace Exam_Cell
         public string Branch { get; set; }
         public string Semester { get; set; }
         public string Course { get; set; }
-
     }
 }
